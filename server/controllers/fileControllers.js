5cfdb9d16e55b662be0e263aa8c4deeae8f19e01
Erbol <macbook@MacBooks-MacBook-Pro.local>
2021-10-22 14:33:23 +0600

    starting adding uploader window

--- server/controllers/fileControllers.js
+++ server/controllers/fileControllers.js
@@ -2,6 +2,9 @@ const fileService = require('../services/fileService');
 const User = require('../models/User');
 const File = require('../models/File');
 
+const config = require('config');
+const fs = require('fs');
+
 class FileController {
   async createDir(req, res) {
     try {
@@ -25,9 +28,112 @@ class FileController {
       return res.status(400).json(error);
     }
   }
-  async fetchFiles(req, res) {
+
+  async getFiles(req, res) {
     try {
-    } catch (error) {}
+      const files = await File.find({
+        user: req.user.id,
+        parent: req.query.parent,
+      });
+      return res.json(files);
+    } catch (error) {
+      console.log('fetch files error', error);
+      return res.status(500).json({ message: 'Can not get files' });
+    }
+  }
+
+  async uploadFile(req, res) {
+    try {
+      const file = req.files.file;
+
+      const parent = await File.findOne({
+        user: req.user.id,
+        _id: req.body.parent,
+      });
+
+      const user = await User.findOne({ _id: req.user.id });
+
+      if (user.usedSpace + file.size > user.diskSpace) {
+        return res.status(400).json({ message: 'There no space on the disk' });
+      } else {
+        user.usedSpace = user.usedSpace + file.size;
+
+        let path;
+
+        if (parent) {
+          path = `${config.get('filePath')}/${user._id}/${parent.path}/${
+            file.name
+          }`;
+        } else {
+          path = `${config.get('filePath')}/${user._id}/${file.name}`;
+        }
+
+        if (fs.existsSync(path)) {
+          return res.status(400).json({ message: 'File already exist' });
+        }
+        file.mv(path);
+
+        const type = file.name.split('.').pop();
+        let filePath = file.name;
+
+        if (parent) {
+          filePath = parent.path + '/' + file.name;
+        }
+
+        const dbFile = new File({
+          name: file.name,
+          type,
+          size: file.size,
+          path: filePath,
+          parent: parent?._id,
+          user: user.id,
+        });
+        await dbFile.save();
+        await user.save();
+
+        res.json(dbFile);
+      }
+    } catch (error) {
+      console.log('error: ', error);
+      return res.status(500).json({ message: 'Upload error' });
+    }
+  }
+
+  async downloadFile(req, res) {
+    try {
+      const file = await File.findOne({ id: req.query.id, user: req.user.id });
+      const path =
+        config.get('filePath') +
+        '/' +
+        req.user.id +
+        '/' +
+        file.path +
+        '/' +
+        file.name;
+      if (fs.existsSync(path)) {
+        return res.download(path, file.name);
+      } else {
+        return res.status(400).json({ message: 'Download error' });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'Load server error' });
+    }
+  }
+
+  async deleteFile(req, res) {
+    try {
+      const file = await File.findOne({ _id: req.query.id, user: req.user.id });
+      if (!file) {
+        return res.status(400).json({ message: 'File for deleting not found' });
+      } else {
+        fileService.deleteFile(file);
+        await file.remove();
+        return res.json({ message: 'File was deleted' });
+      }
+    } catch (error) {
+      // console.log('ewqeqweeqw', error);
+      return res.status(400).json({ message: 'Dir is not empty' });
+    }
   }
 }
 
